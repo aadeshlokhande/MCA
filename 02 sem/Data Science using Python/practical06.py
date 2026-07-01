fe445b8e8c6ad9be27878a18d2a792e6d4fb9441
Adesh <aadeshlokhande11@gmail.com>
2026-07-01 09:20:19 +0530

    update

---
+++ 02 sem/Data Science using Python/practical06.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr1 = np.array([10,20,30,40,50])
+arr2 = np.array([1,2,3,4,5])
+print(f"First Arr = {arr1}")
+print(f"Second Arr = {arr2}")
+
+print(f"Addition = {arr1+arr2}")
+print(f"Multiplication = {arr1*arr2}")
+
+print(f"Mean = {np.mean(arr1)}")
+print(f"Median = {np.median(arr1)}")
+print(f"Standard Deviation = {np.std(arr1)}")
